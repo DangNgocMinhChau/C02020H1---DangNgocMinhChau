fea4349216efdfe7322fb6de8de62ead5809ab97
unknown <chaudang1595@gmail.com>
2020-02-27 07:07:04 +0700

    'ThucHanh'

---
+++ module 1/#2BienKeiDuLieuVaToanTu/BaiTap/UngDungChuyenDoiTienTe/text.js
@@ -0,0 +1,10 @@
+.class {
+    let inputnumber;
+    const b = 20000;
+    inputnumber = prompt(" Nhập số tiền vào đây :");
+    let a = parseInt(inputnumber);
+    let CD = a * b;
+    document.write("số tiền chuyển đổi là : =" + CD);
+
+}
+
