c67246a5943759f63afb41a70848478013ce850e
unknown <chaudang1595@gmail.com>
2020-02-28 09:47:45 +0700

    'baitap'

---
+++ module 1/#2BienKieuDuLieuVaToanTu/BaiTap/BaiTapMayTinh/text.js
@@ -0,0 +1,13 @@
+function show() {
+    var numberQne = document.getElementById("numberQne");
+    var numberTwo = document.getElementById("numberTwo");
+    var addition = document .getElementById("addition");
+    var Subtraction =document.getElementById("Subtraction");
+    var Multiplication =document.getElementById("Multiplication");
+    var Division = document.getElementById("Division");
+    var result =document.getElementById("result");
+    var total;
+
+
+
+}
--- module 1/#2BienKieuDuLieuVaToanTu/BaiTap/UngDungChuyenDoiTienTe/text.js
+++ module 1/#2BienKieuDuLieuVaToanTu/BaiTap/UngDungChuyenDoiTienTe/text.js
@@ -1,10 +1,14 @@
-.class {
-    let inputnumber;
-    const b = 20000;
-    inputnumber = prompt(" Nhập số tiền vào đây :");
-    let a = parseInt(inputnumber);
-    let CD = a * b;
-    document.write("số tiền chuyển đổi là : =" + CD);
-
+function show() {
+    var number = document.getElementById("number").value;
+    var toCurrency =document .getElementById("toCurrency");
+    var fromCurrency = document.getElementById("fromCurrency");
+    var result = document.getElementById("result");
+    var total
+    if(fromCurrency.value === "VietNam" && toCurrency.value ==="USD"){
+            total = number / 23000;
+    }
+    else if (fromCurrency.value === "USD" && toCurrency.value ==="VietNam"){
+            total = number * 23000;
+    }
+            result.innerText=total;
 }
-
