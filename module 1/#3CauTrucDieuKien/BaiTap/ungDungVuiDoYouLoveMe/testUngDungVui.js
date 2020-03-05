eaeab0c3cde68bf30188651dc3bcda6719ff6b76
unknown <chaudang1595@gmail.com>
2020-03-05 22:04:23 +0700

    'BaiTAp

---
+++ module 1/#3CauTrucDieuKien/BaiTap/ungDungVuiDoYouLoveMe/testUngDungVui.js
@@ -0,0 +1,9 @@
+function yesClick() {
+        alert("yes.you are right")
+}
+function noMouse() {
+        var x = Math.round(Math.random() * window.innerWidth);
+        var y = Math.round(Math.round() * window.innerHeight);
+        document.getElementById("btnNo").style.left = x + 'px';
+        document.getElementById("btnNo").style.top = y + 'px';
+}
